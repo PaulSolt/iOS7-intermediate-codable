dfa7d0af07f91e09235f88c444ca86b530c8f982
Paul Solt <PaulSolt@gmail.com>
2019-08-05 12:36:05 -0400

    Added initial logic

--- iOS7-StarWars.playground/Contents.swift
+++ iOS7-StarWars.playground/Contents.swift
@@ -1,2 +1,26 @@
 import UIKit
 
+// https://swapi.co/api/people/1/
+
+struct Person: Codable {
+    let name: String
+    let height: Int // Int vs. String
+    let hairColor: String // hair_color vs. hairColor
+    
+    let films: [URL]
+    let vehicles: [URL]
+    let starships: [URL]
+    
+    init(from decoder: Decoder) throws {
+        
+    }
+    
+}
+
+// Prototyping code: please handle errors in a real app
+let url = URL(string: "https://swapi.co/api/people/1/")!
+let data = try! Data(contentsOf: url)
+
+let decoder = JSONDecoder()
+let luke = try! decoder.decode(Person.self, from: data)
+print(luke)
