008b732364e8217b254efb2e1d1c1cebeb4ec3fb
Paul Solt <PaulSolt@gmail.com>
2019-08-05 13:01:21 -0400

    Added parsing films and fixed bugs with API not parsing type correctly or snake case

--- iOS7-StarWars.playground/Contents.swift
+++ iOS7-StarWars.playground/Contents.swift
@@ -6,13 +6,42 @@ struct Person: Codable {
     let name: String
     let height: Int // Int vs. String
     let hairColor: String // hair_color vs. hairColor
-    
+
     let films: [URL]
-    let vehicles: [URL]
-    let starships: [URL]
+//    let vehicles: [URL]
+//    let starships: [URL]
+    
+    enum PersonKeys: String, CodingKey {
+        case name
+        case height // We can rename variables: = "height_in_meters"
+        case hairColor = "hair_color"
+        case films
+        case vehicles
+        case starships
+    }
+    
     
     init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PersonKeys.self)
         
+        name = try container.decode(String.self, forKey: .name)
+        
+        let heightString = try container.decode(String.self, forKey: .height)
+        height = Int(heightString) ?? 0
+        
+        hairColor = try container.decode(String.self, forKey: .hairColor)
+        
+        var filmsContainer = try container.nestedUnkeyedContainer(forKey: .films)
+        
+        // 1st approach using a while loop
+        var filmURLs: [URL] = []
+        while filmsContainer.isAtEnd == false {
+            let filmString = try filmsContainer.decode(String.self)
+            if let url = URL(string: filmString) {
+                filmURLs.append(url)
+            }
+        }
+        films = filmURLs
     }
     
 }
