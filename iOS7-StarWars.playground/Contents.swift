f8dff48ffd0d742bb1dab978114fec6119af1f59
Paul Solt <PaulSolt@gmail.com>
2019-08-05 14:44:18 -0400

    Added a fallback approach if value is a num/string to illustrate more logic (commented out). Added the ability to export to JSON or XML (or binary in comments)

--- iOS7-StarWars.playground/Contents.swift
+++ iOS7-StarWars.playground/Contents.swift
@@ -29,6 +29,15 @@ struct Person: Codable {
         let heightString = try container.decode(String.self, forKey: .height)
         height = Int(heightString) ?? 0
         
+        // Back up decoding method if the types don't match
+//        if let heightString = try? container.decode(String.self, forKey: .height) {
+//            self.height = Int(heightString) ?? 0
+//        } else if let height = try? container.decode(Int.self, forKey: .height) {
+//            self.height = height
+//        } else {
+//            self.height = 0
+//        }
+        
         hairColor = try container.decode(String.self, forKey: .hairColor)
         
         var filmsContainer = try container.nestedUnkeyedContainer(forKey: .films)
@@ -53,6 +62,28 @@ struct Person: Codable {
         
     }
     
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: PersonKeys.self)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(String(height), forKey: .height)
+        try container.encode(hairColor, forKey: .hairColor)
+        
+        // 1st approach
+        var filmsContainer = container.nestedUnkeyedContainer(forKey: .films)
+        for filmURL in films {
+            try filmsContainer.encode(filmURL.absoluteString)
+        }
+        
+        // 2nd approach
+        let vehicleStrings = vehicles.map { $0.absoluteString }
+        try container.encode(vehicleStrings, forKey: .vehicles)
+        
+        // 3rd approach
+        try container.encode(starships, forKey: .starships)
+    }
+    
+    
 }
 
 // Prototyping code: please handle errors in a real app
@@ -62,3 +93,30 @@ let data = try! Data(contentsOf: url)
 let decoder = JSONDecoder()
 let luke = try! decoder.decode(Person.self, from: data)
 print(luke)
+
+let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+let lukeData = try! encoder.encode(luke)
+
+// print out data as Sting
+let lukeString = String(data: lukeData, encoding: .utf8)
+print(lukeString!)
+
+let testLuke = try! decoder.decode(Person.self, from: lukeData)
+print(testLuke)
+
+
+
+let plistEncoder = PropertyListEncoder()
+plistEncoder.outputFormat = .xml // .binary // .xml
+
+let plistData = try plistEncoder.encode(luke)
+
+// Print .binary data using NSData in a print
+//let newData = NSData(data: plistData)
+//print(newData)
+
+let plistString = String(data: plistData, encoding: .utf8)!
+print(plistString)
+
+//plistData.write(to: <#T##URL#>) // save to disk
